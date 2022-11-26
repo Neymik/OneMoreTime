0a6e8e5716a8edb8f9de881887644f77f8011242
Vadim <vademcheg@gmail.com>
2022-11-26 21:51:13 +0400

    Add test dialogic popup

--- player.gd
+++ player.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
+
+export var dialogic_popup_scene = "res://Scenes/dialogicPopup.tscn"
+
 var velocity: Vector2 = Vector2()
 var direction: Vector2 = Vector2()
+
 func read_input (): 
 	velocity = Vector2()
 
@@ -23,7 +27,12 @@ func read_input ():
 func _physics_process(_delta):
 	
 	# check collisions
-	for i in get_slide_count():
+	var slide_count = get_slide_count()
+	
+	if !slide_count:
+		pass
+	
+	for i in slide_count:
 		var collision = get_slide_collision(i)
 		
 		if collision.collider.name == "TileMap":
@@ -31,11 +40,24 @@ func _physics_process(_delta):
 			var pos = tileMap.world_to_map(collision.position)
 			var id = tileMap.get_cellv(pos)
 			
-			var dialog = Dialogic.start("timeline-colly")
-			Dialogic.set_variable("collision_definition", "TileMap " + str(pos) + " " + str(id))
-			add_child(dialog)
+			var dialogic_popup = get_node("dialogic_popup")
+			if dialogic_popup:
+				remove_child(dialogic_popup)
+			
+			dialogic_popup = load(dialogic_popup_scene).instance()
+			add_child(dialogic_popup)
+			dialogic_popup.name = "dialogic_popup"
+			dialogic_popup.popup_data = "TileMap " + str(pos) + " " + str(id)
+			dialogic_popup.get_node("Button").text = "Button for " + str(pos)
+			dialogic_popup.connect("popup_clicked", self, "_on_popup_clicked")
 			
 			print()
 			# tileMap.get_cellv(tileMap.world_to_map(coll_pos))
 	
 	read_input()
+
+func _on_popup_clicked(popup_self):
+	var dialog = Dialogic.start("timeline-colly")
+	Dialogic.set_variable("collision_definition", popup_self.popup_data)
+	remove_child(popup_self)
+	add_child(dialog)
